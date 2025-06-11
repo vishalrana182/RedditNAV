117119c1e0bbb5fa1e8f9b7779291deba0fa1730
vishal singh rana <vishalrana01082001@gmail.com>
2025-06-11 08:06:54 +0530

    finishing

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-function findButtonInLightbox(label) {
-    const lightbox = document.querySelector('#shreddit-media-lightbox');
-    if (!lightbox) return null;
-    const template = lightbox.querySelector('div[data-testid="lightbox-template"]');
-    if (!template) return null;
-    const carousel = template.querySelector('gallery-carousel');
+function func(label) {
+    const light = document.querySelector('#shreddit-media-lightbox');
+    if (!light) return null;
+    const temp = light.querySelector('div[data-testid="lightbox-template"]');
+    if (!temp) return null;
+    const carousel = temp.querySelector('gallery-carousel');
     if (!carousel || !carousel.shadowRoot) return null;
     return carousel.shadowRoot.querySelector(`button[aria-label="${label}"]`);
 }
 
 document.addEventListener("keydown", function(k) {
     if (k.key === "ArrowRight") {
-        const nextBtn = findButtonInLightbox("Next page");
+        const nextBtn = func("Next page");
         if (nextBtn) {
             nextBtn.click();
         }
     }
     if (k.key === "ArrowLeft") {
-        const prevBtn = findButtonInLightbox("Previous page");
+        const prevBtn = func("Previous page");
         if (prevBtn) {
             prevBtn.click();
         }
